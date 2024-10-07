ee636962b94b03da6a6685bdce07baa195f333e0
Zayne-Orosz <zayne.orosz@gmail.com>
2024-10-06 20:10:12 -0400

    Create Project 4.cs

---
+++ Module_4/Project 4.cs
@@ -0,0 +1,53 @@
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("This program will divide the first number by the second! Input the first number")
+      string input1 = Console.Readline();
+
+      Console.WriteLine("Input the second number now!")
+      string input2 = Console.ReadLine();
+
+      try
+      {
+          int number1 = Convert.ToInt32(input1);
+          int number2 = Convert.ToInt32(input2);
+
+          int answer = Divide(number1, number2);
+          Console.WriteLine($"{number1} divided by {number2} equals to {answer}");
+
+      }
+      catch (FormatException ex)
+      {
+          Console.WriteLine("Error: One of your two inputs was not a number! Please restart and try again.");
+          Console.WriteLine($"More detailed information: {ex.Message}");
+      }
+      catch (DivideByZeroException ex)
+      {
+          Console.WriteLine("You cannot divide by zero, unfortunately! Please restart and try again.");
+          Console.WriteLine($"More detailed information: {ex.Message}");
+      }
+      catch (Exception ex)
+      {
+          Console.WriteLine("An unexpected error occurred! Please restart and try again.");
+          Console.WriteLine($"More detailed information: {ex.Message}");
+      }
+      catch (OverflowException ex)
+      {
+          Console.WriteLine("The numbers inputted became too big and caused a stack overflow! Please restart and try again.");
+          Console.WriteLine($"More detailed information: {ex.Message}");
+      }
+      catch (NotFiniteNumber ex)
+      {
+          Console.WriteLine("You cannot use infinity or nonintegers for this program. Please restart and try again.");
+          Console.WriteLine($"More detailed information: {ex.Message}");
+      }
+
+      Console.WriteLine("Press any key to end the program.");
+      Console.Readkey();
+  }
+
+  static int Divide(int a, int b)
+  {
+      return a / b;
